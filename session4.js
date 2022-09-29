acab6b7ba854c50c712e1152dad45f939b6683bb
Haroon Nawaz <haroonnawaz@yahoo.com>
2022-09-29 23:54:03 +0500

    first commit

---
+++ session4.js
@@ -0,0 +1,66 @@
+//Method 1
+// function makeCapital(var1:number ){
+//     console.log(var1.toFixed())
+// }
+// let input = Number(prompt("enter the text to make it capital"));
+// makeCapital(input)
+//Method 2 
+// let func_name = () => {
+//     return "Welcom"
+// }
+///**************
+// let func_name = () =>  console.log("Welcom")
+// function func_name (num1: number, text: string){
+//     if (num1 > 5){
+//         return 1
+//     }
+//     else 
+// return "Hello World"
+// }
+// ********************
+// type person = {
+//     age: number,
+//     name: string
+// }
+// let func_name = (): person => {
+//     return {
+//         age: 30,
+//         name: "my name "
+//     }
+// }
+//********************* */
+// const variable_name = "Pakistan"; ///literals 
+//callback 
+// function func_1(callback: Function){
+//     callback()
+//     console.log("Hurrah")
+//     }
+// function func_2(){
+//     console.log("I have passed my quiz")
+// }
+// func_1(func_2)
+//*************************************
+// //please wait , installing
+// ..
+// //operation completed successfully 
+//**************************************** */
+//fuction overloading 
+// function add(input1: number, input2: string)
+// function add(input1: string, input2: number)
+// function add(input1: number, input2: number)
+// function add(input1:any, input2: any) //implementation
+// {
+// return input1 + input2
+// }
+// console.log(add(2, 2))
+// console.log(add(2, "2"))
+//
+var var1 = "";
+var arr = [1, 2, 3, "Karchi", false, null, 1, 23, ""];
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var i = arr_1[_i];
+    console.log(i);
+}
+//tuple 
+// let tuple_var: [number,number, string, boolean] = [3, 1, "st",false]
+// 
---
+++ session4.ts
@@ -0,0 +1,94 @@
+//Method 1 - Declaring Functions
+
+// function makeCapital(var1:number ){
+   
+//     console.log(var1.toFixed())
+// }
+// let input = Number(prompt("enter the text to make it capital"));
+// makeCapital(input)
+
+//Method 2 ************
+
+// let func_name = () => {
+//     return "Welcom"
+        
+// }
+
+///**************
+//Method3 ********************
+// let func_name = () =>  console.log("Welcom")
+//Method4 ********************
+// function func_name (num1: number, text: string){
+
+//     if (num1 > 5){
+//         return 1
+//     }
+    
+//     else 
+// return "Hello World"
+// }
+// ********************
+
+// type person = {
+//     age: number,
+//     name: string
+// }
+// let func_name = (): person => {
+//     return {
+//         age: 30,
+//         name: "my name "
+//     }
+// }
+//********************* */
+// const variable_name = "Pakistan"; ///literals 
+//callback 
+// function func_1(callback: Function){
+//     callback()
+
+//     console.log("Hurrah")
+
+//     }
+// function func_2(){
+//     console.log("I have passed my quiz")
+// }
+
+// func_1(func_2)
+
+//*************************************
+
+// //please wait , installing
+// ..
+// //operation completed successfully 
+
+//**************************************** */
+//fuction overloading 
+
+// function add(input1: number, input2: string)
+// function add(input1: string, input2: number)
+
+// function add(input1: number, input2: number)
+
+
+// function add(input1:any, input2: any) //implementation
+// {
+// return input1 + input2
+
+// }
+// console.log(add(2, 2))
+// console.log(add(2, "2"))
+
+// //
+// let var1= ""
+// let arr = [1, 2, 3, "Karchi", false, null, 1,23, ""]
+
+// for( let i of arr)
+// console.log(i)
+
+
+//tuple 
+
+// let tuple_var: [number,number, string, boolean] = [3, 1, "st",false]
+// 
+
+
+
